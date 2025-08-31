859d2ce2e439bb024e5f9602b15214cdc9b687fb
Hakula Chen <i@hakula.xyz>
2025-08-31 21:09:33 +0800

    refactor(binding/containers): extract common logic to a template function

--- binding/containers_binding.cpp
+++ binding/containers_binding.cpp
@@ -6,14 +6,17 @@
 #include <format>
 #include <functional>
 #include <initializer_list>
+#include <ranges>
 #include <string>
 #include <string_view>
+#include <utility>
 #include <vector>
 
 #include <pybind11/attr.h>
 #include <pybind11/detail/common.h>
 #include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
 #include <pybind11/stl.h>
 
 #include "concepts/concepts.hpp"
@@ -35,10 +38,17 @@ auto GetItem(const Container<T> &self, typename Container<T>::size_type index) -
   throw py::index_error("Index out of bounds");
 }
 
-template <typename Input, typename Output>
+template <typename T>
+auto GetFilteredWrapper(const Container<T> &self, const std::function<bool(T)> &filter) {
+  auto filtered_view = self.template GetFilteredView<std::function<bool(T)>>(filter);
+  return std::ranges::to<std::vector>(std::move(filtered_view));
+}
+
+template <typename Input>
 auto GetTransformWrapper(const Container<Input> &self,
-                         const std::function<Output(Input)> &transform) {
-  return std::ranges::to<std::vector>(self.template GetTransformedView<Output>(transform));
+                         const std::function<py::object(Input)> &transform) {
+  auto transformed_view = self.template GetTransformedView<py::object>(transform);
+  return std::ranges::to<std::vector>(std::move(transformed_view));
 }
 
 template <CopyableType T>
@@ -47,90 +57,40 @@ auto GetIter(const Container<T> &self) {
 }
 
 template <CopyableType T>
-auto GetRepr(const Container<T> &self, std::string_view type_name) {
-  return std::format("<{}Container(size={}) at {}>", type_name, self.GetSize(),
+auto GetRepr(const Container<T> &self, std::string_view class_name) {
+  return std::format("<{}(size={}) at {}>", class_name, self.GetSize(),
                      static_cast<const void *>(&self));
+}
+
+// Template function to register container bindings for a specific type
+template <typename T>
+void BindContainerFor(py::module &m, std::string_view type_name) {
+  auto class_name = std::format("{}Container", type_name);
+  py::class_<Container<T>>(m, class_name.c_str())
+      .def(py::init<>())
+      .def(py::init<typename Container<T>::size_type>())
+      .def(py::init<std::initializer_list<T>>())
+      .def(py::init<std::vector<T>>())
+      .def("add", py::overload_cast<const T &>(&Container<T>::Add))
+      .def("remove", &Container<T>::Remove)
+      .def("size", &Container<T>::GetSize)
+      .def("empty", &Container<T>::IsEmpty)
+      .def("at", &GetItem<T>)
+      .def("view", &Container<T>::GetView)
+      .def("filter", &GetFilteredWrapper<T>)
+      .def("transform", &GetTransformWrapper<T>)
+      .def("__len__", &Container<T>::GetSize)
+      .def("__bool__", [](const Container<T> &self) { return !self.IsEmpty(); })
+      .def("__getitem__", &GetItem<T>)
+      .def("__iter__", &GetIter<T>, py::keep_alive<0, 1>())
+      .def("__str__", [](const Container<T> &self) { return std::format("{}", self); })
+      .def("__repr__", [&](const Container<T> &self) { return GetRepr(self, class_name); });
 }
 
 }  // namespace
 
 void BindContainers(py::module &m) {
-  using IntContainer = Container<int>;
-  using FloatContainer = Container<double>;
-  using StringContainer = Container<std::string>;
-
-  // Bind ContainerError enum
-  py::enum_<ContainerError>(m, "ContainerError")
-      .value("EMPTY", ContainerError::kEmpty)
-      .value("OUT_OF_BOUNDS", ContainerError::kOutOfBounds)
-      .value("INVALID_OPERATION", ContainerError::kInvalidOperation);
-
-  // Bind Container<int>
-  py::class_<IntContainer>(m, "IntContainer")
-      .def(py::init<>())
-      .def(py::init<IntContainer::size_type>())
-      .def(py::init<std::initializer_list<int>>())
-      .def(py::init<std::vector<int>>())
-      .def("add", py::overload_cast<const int &>(&IntContainer::Add))
-      .def("remove", &IntContainer::Remove)
-      .def("size", &IntContainer::GetSize)
-      .def("empty", &IntContainer::IsEmpty)
-      .def("at", &GetItem<int>)
-      .def("view", &IntContainer::GetView)
-      .def("filter", &IntContainer::GetFilteredView<std::function<bool(int)>>)
-      .def("transform", &GetTransformWrapper<int, int>)
-      .def("transform", &GetTransformWrapper<int, double>)
-      .def("transform", &GetTransformWrapper<int, std::string>)
-      .def("__len__", &IntContainer::GetSize)
-      .def("__bool__", [](const IntContainer &self) { return !self.IsEmpty(); })
-      .def("__getitem__", &GetItem<int>)
-      .def("__iter__", &GetIter<int>, py::keep_alive<0, 1>())
-      .def("__str__", [](const IntContainer &self) { return std::format("{}", self); })
-      .def("__repr__", [](const IntContainer &self) { return GetRepr(self, "Int"); });
-
-  // Bind Container<double>
-  py::class_<FloatContainer>(m, "FloatContainer")
-      .def(py::init<>())
-      .def(py::init<FloatContainer::size_type>())
-      .def(py::init<std::initializer_list<double>>())
-      .def(py::init<std::vector<double>>())
-      .def("add", py::overload_cast<const double &>(&FloatContainer::Add))
-      .def("remove", &FloatContainer::Remove)
-      .def("size", &FloatContainer::GetSize)
-      .def("empty", &FloatContainer::IsEmpty)
-      .def("at", &GetItem<double>)
-      .def("view", &FloatContainer::GetView)
-      .def("filter", &FloatContainer::GetFilteredView<std::function<bool(double)>>)
-      .def("transform", &GetTransformWrapper<double, int>)
-      .def("transform", &GetTransformWrapper<double, double>)
-      .def("transform", &GetTransformWrapper<double, std::string>)
-      .def("__len__", &FloatContainer::GetSize)
-      .def("__bool__", [](const FloatContainer &self) { return !self.IsEmpty(); })
-      .def("__getitem__", &GetItem<double>)
-      .def("__iter__", &GetIter<double>, py::keep_alive<0, 1>())
-      .def("__str__", [](const FloatContainer &self) { return std::format("{}", self); })
-      .def("__repr__", [](const FloatContainer &self) { return GetRepr(self, "Float"); });
-
-  // Bind Container<std::string>
-  py::class_<StringContainer>(m, "StringContainer")
-      .def(py::init<>())
-      .def(py::init<StringContainer::size_type>())
-      .def(py::init<std::initializer_list<std::string>>())
-      .def(py::init<std::vector<std::string>>())
-      .def("add", py::overload_cast<const std::string &>(&StringContainer::Add))
-      .def("remove", &StringContainer::Remove)
-      .def("size", &StringContainer::GetSize)
-      .def("empty", &StringContainer::IsEmpty)
-      .def("at", &GetItem<std::string>)
-      .def("view", &StringContainer::GetView)
-      .def("filter", &StringContainer::GetFilteredView<std::function<bool(const std::string &)>>)
-      .def("transform", &GetTransformWrapper<std::string, int>)
-      .def("transform", &GetTransformWrapper<std::string, double>)
-      .def("transform", &GetTransformWrapper<std::string, std::string>)
-      .def("__len__", &StringContainer::GetSize)
-      .def("__bool__", [](const StringContainer &self) { return !self.IsEmpty(); })
-      .def("__getitem__", &GetItem<std::string>)
-      .def("__iter__", &GetIter<std::string>, py::keep_alive<0, 1>())
-      .def("__str__", [](const StringContainer &self) { return std::format("{}", self); })
-      .def("__repr__", [](const StringContainer &self) { return GetRepr(self, "String"); });
+  BindContainerFor<int>(m, "Int");
+  BindContainerFor<double>(m, "Float");
+  BindContainerFor<std::string>(m, "String");
 }
